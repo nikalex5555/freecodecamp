17d6b03bc14a043d9e315efe93b8b8f0f714e6bb
nikalex5555 <nikalex5555@gmail.com>
2021-12-03 17:52:39 +0300

    Manipulating Complex Objects

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ function checkObj(obj, checkProp) {
   // Only change code above this line
 }
 console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "gift"));
+
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    ['new'],
+    "gold": true
+  },
+  {
+     "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+      ],
+  }
+];
+myMusic['new'];
