4ee187246a499cdbadc0fd58e73628fd8e9f23d4
nikalex5555 <nikalex5555@gmail.com>
2021-12-04 04:04:10 +0300

    Iterate with JavaScript While Loops

--- script.js
+++ script.js
@@ -73,3 +73,48 @@ const myPlants = [
 ];
 
 const secondTree = myPlants[1].list[1];
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(value === ""){
+    delete recordCollection[id][prop];}
+  else if(prop==='tracks'){recordCollection[id][prop]=recordCollection[id][prop]||[];
+    recordCollection[id][prop].push(value);
+  }
+  else{
+    recordCollection[id][prop]=value;
+  }
+  return recordCollection;
+}
+console.log(updateRecords(recordCollection, 2548, "artist", ""));
+console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+
+
+const myArray = [];
+let i=5;
+while(i>=0){
+  myArray.push(i);
+i--;
+}
